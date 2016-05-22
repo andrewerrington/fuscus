ff2f534a9d7022dd9bf653d4d7d8b26f350925f5
Andrew Errington <erringtona@gmail.com>
2016-05-22 12:36:38 +0900

    Converted tabs to spaces.

--- fuscus/displayLCD.py
+++ fuscus/displayLCD.py
@@ -49,230 +49,230 @@ stateOnDisplay = None
 
 
 def init():
-	#stateOnDisplay = 0xFF; // set to unknown state to force update
-	#flags = LCD_FLAG_ALTERNATE_ROOM
-	#lcd.init(); // initialize LCD
-	#lcd.begin(20, 4)
-	#lcd.clear()
-	pass	#FIXME this code needs to do something. Probably.
+    #stateOnDisplay = 0xFF; // set to unknown state to force update
+    #flags = LCD_FLAG_ALTERNATE_ROOM
+    #lcd.init(); // initialize LCD
+    #lcd.begin(20, 4)
+    #lcd.clear()
+    pass    #FIXME this code needs to do something. Probably.
 
 
 def printStationaryText():
-	"""Print the stationary text on the lcd."""
-	
-	LCD.printat(0, 0, "Mode   ")
-	LCD.printat(0, 1, STR_Beer_)
+    """Print the stationary text on the lcd."""
 
-	LCD.printat(0, 2, "Room  " if (flags & LCD_FLAG_DISPLAY_ROOM) else STR_Fridge_)
-	
-	# The Nokia screen is not wide enough for this, but the
-	# web interface will show it
-	printDegreeUnit(18, 1)
-	printDegreeUnit(18, 2)
+    LCD.printat(0, 0, "Mode   ")
+    LCD.printat(0, 1, STR_Beer_)
+
+    LCD.printat(0, 2, "Room  " if (flags & LCD_FLAG_DISPLAY_ROOM) else STR_Fridge_)
+
+    # The Nokia screen is not wide enough for this, but the
+    # web interface will show it
+    printDegreeUnit(18, 1)
+    printDegreeUnit(18, 2)
 
 
 def printDegreeUnit(x, y):
-	"""Print degree sign + temp unit."""
-	LCD.cursor(x,y)
-	LCD.print('°')
-	LCD.print(tempControl.cc.tempFormat)
+    """Print degree sign + temp unit."""
+    LCD.cursor(x,y)
+    LCD.print('°')
+    LCD.print(tempControl.cc.tempFormat)
 
 
 def printMode():
-	"""Print mode on the right location on the first line, after "Mode   "."""
-	LCD.printat(7, 0, ' '*13)
-	LCD.cursor(7,0)
+    """Print mode on the right location on the first line, after "Mode   "."""
+    LCD.printat(7, 0, ' '*13)
+    LCD.cursor(7,0)
 
-	if (tempControl.getMode() == MODES['MODE_FRIDGE_CONSTANT']):
-		LCD.print(STR_Fridge_)
-		LCD.print(STR_Const_)
-			
-	elif (tempControl.getMode() == MODES['MODE_BEER_CONSTANT']):
-		LCD.print(STR_Beer_)
-		LCD.print(STR_Const_)
-			
-	elif (tempControl.getMode() == MODES['MODE_BEER_PROFILE']):
-		LCD.print(STR_Beer_)
-		LCD.print("Profile")
-			
-	elif (tempControl.getMode() == MODES['MODE_OFF']):
-		LCD.print("Off")
-			
-	elif (tempControl.getMode() == MODES['MODE_TEST']):
-		LCD.print("** Testing **")
-			
-	else:
-		LCD.print("Invalid mode")
-			
-	#lcd.printSpacesToRestOfLine();
+    if (tempControl.getMode() == MODES['MODE_FRIDGE_CONSTANT']):
+        LCD.print(STR_Fridge_)
+        LCD.print(STR_Const_)
+
+    elif (tempControl.getMode() == MODES['MODE_BEER_CONSTANT']):
+        LCD.print(STR_Beer_)
+        LCD.print(STR_Const_)
+
+    elif (tempControl.getMode() == MODES['MODE_BEER_PROFILE']):
+        LCD.print(STR_Beer_)
+        LCD.print("Profile")
+
+    elif (tempControl.getMode() == MODES['MODE_OFF']):
+        LCD.print("Off")
+
+    elif (tempControl.getMode() == MODES['MODE_TEST']):
+        LCD.print("** Testing **")
+
+    else:
+        LCD.print("Invalid mode")
+
+    #lcd.printSpacesToRestOfLine();
 
 
 def printState():
-	"""Print the current state on the last line of the LCD."""
+    """Print the current state on the last line of the LCD."""
 
-	global stateOnDisplay
+    global stateOnDisplay
 
-	time = None
-	state = tempControl.getDisplayState()
-	
-	if (state != stateOnDisplay):	# only print static text when state has changed
-		stateOnDisplay = state
-		# Reprint state and clear rest of the line
-		
-		LCD.printat(0, 3, ' '*20)	# Actually, clear line then overprint
-		
-		part1 = STR_empty_string
-		part2 = STR_empty_string
-		
-		if (state == STATES['IDLE']):
-			part1 = "Idl"
-			part2 = STR_ing_for
-		elif (state == STATES['WAITING_TO_COOL']):
-			part1 = STR_Wait_to_
-			part2 = STR_Cool
-		elif (state == STATES['WAITING_TO_HEAT']):
-			part1 = STR_Wait_to_
-			part2 = STR_Heat
-		elif (state == STATES['WAITING_FOR_PEAK_DETECT']):
-			part1 = "Waiting for peak"
-		elif (state == STATES['COOLING']):
-			part1 = STR_Cool
-			part2 = STR_ing_for
-		elif (state == STATES['HEATING']):
-			part1 = STR_Heat
-			part2 = STR_ing_for
-		elif (state == STATES['COOLING_MIN_TIME']):
-			part1 = STR_Cool
-			part2 = STR__time_left
-		elif (state == STATES['HEATING_MIN_TIME']):
-			part1 = STR_Heat
-			part2 = STR__time_left
-		elif (state == STATES['DOOR_OPEN']):
-			part1 = "Door open"
-		elif (state == STATES['STATE_OFF']):
-			part1 = "Temp. control OFF"
-		else:
-			part1 = "Unknown status!"
-		
-		LCD.printat(0, 3, part1)
-		LCD.print(part2)
-		#lcd.printSpacesToRestOfLine();
+    time = None
+    state = tempControl.getDisplayState()
 
-	sinceIdleTime = tempControl.timeSinceIdle()
-	
-	if (state == STATES['IDLE']):
-		time = min(tempControl.timeSinceCooling(), tempControl.timeSinceHeating())
-	elif state in (STATES['COOLING'], STATES['HEATING']):
-		time = sinceIdleTime
-	elif (state == STATES['COOLING_MIN_TIME']):
-		time = MIN_COOL_ON_TIME - sinceIdleTime
-	elif (state == STATES['HEATING_MIN_TIME']):
-		time = MIN_HEAT_ON_TIME - sinceIdleTime
-	elif state in (STATES['WAITING_TO_COOL'], STATES['WAITING_TO_HEAT']):
-		time = tempControl.getWaitTime()
+    if (state != stateOnDisplay):   # only print static text when state has changed
+        stateOnDisplay = state
+        # Reprint state and clear rest of the line
 
-	if (time is not None):
-		minutes = time / 60
-		hours = minutes / 60
-		# Nokia LCD is 17 characters wide, so bring nnmnn
-		# 3 characters left by padding with spaces on the
-		# right so we can see.
-		printString="%dm%02d   "%(minutes, time%60)
+        LCD.printat(0, 3, ' '*20)   # Actually, clear line then overprint
 
-		# If we have hours, then mnn seconds will not be
-		# visible on LCD, but will be visible on web interface.
-		if (int(hours) != 0):
-			printString="%2dh%02dm%02d"%(hours, minutes%60, time%60)
+        part1 = STR_empty_string
+        part2 = STR_empty_string
 
-		LCD.printat(20-len(printString), 3, printString)
+        if (state == STATES['IDLE']):
+            part1 = "Idl"
+            part2 = STR_ing_for
+        elif (state == STATES['WAITING_TO_COOL']):
+            part1 = STR_Wait_to_
+            part2 = STR_Cool
+        elif (state == STATES['WAITING_TO_HEAT']):
+            part1 = STR_Wait_to_
+            part2 = STR_Heat
+        elif (state == STATES['WAITING_FOR_PEAK_DETECT']):
+            part1 = "Waiting for peak"
+        elif (state == STATES['COOLING']):
+            part1 = STR_Cool
+            part2 = STR_ing_for
+        elif (state == STATES['HEATING']):
+            part1 = STR_Heat
+            part2 = STR_ing_for
+        elif (state == STATES['COOLING_MIN_TIME']):
+            part1 = STR_Cool
+            part2 = STR__time_left
+        elif (state == STATES['HEATING_MIN_TIME']):
+            part1 = STR_Heat
+            part2 = STR__time_left
+        elif (state == STATES['DOOR_OPEN']):
+            part1 = "Door open"
+        elif (state == STATES['STATE_OFF']):
+            part1 = "Temp. control OFF"
+        else:
+            part1 = "Unknown status!"
+
+        LCD.printat(0, 3, part1)
+        LCD.print(part2)
+        #lcd.printSpacesToRestOfLine();
+
+    sinceIdleTime = tempControl.timeSinceIdle()
+
+    if (state == STATES['IDLE']):
+        time = min(tempControl.timeSinceCooling(), tempControl.timeSinceHeating())
+    elif state in (STATES['COOLING'], STATES['HEATING']):
+        time = sinceIdleTime
+    elif (state == STATES['COOLING_MIN_TIME']):
+        time = MIN_COOL_ON_TIME - sinceIdleTime
+    elif (state == STATES['HEATING_MIN_TIME']):
+        time = MIN_HEAT_ON_TIME - sinceIdleTime
+    elif state in (STATES['WAITING_TO_COOL'], STATES['WAITING_TO_HEAT']):
+        time = tempControl.getWaitTime()
+
+    if (time is not None):
+        minutes = time / 60
+        hours = minutes / 60
+        # Nokia LCD is 17 characters wide, so bring nnmnn
+        # 3 characters left by padding with spaces on the
+        # right so we can see.
+        printString="%dm%02d   "%(minutes, time%60)
+
+        # If we have hours, then mnn seconds will not be
+        # visible on LCD, but will be visible on web interface.
+        if (int(hours) != 0):
+            printString="%2dh%02dm%02d"%(hours, minutes%60, time%60)
+
+        LCD.printat(20-len(printString), 3, printString)
 
 
 def printAllTemperatures():
-	"""Print all temperatures on the LCD."""
+    """Print all temperatures on the LCD."""
 
-	global flags
+    global flags
 
-	# alternate between beer and room temp
-	if (flags & LCD_FLAG_ALTERNATE_ROOM):
-	#	bool displayRoom = ((ticks.seconds()&0x08)==0) && !BREWPI_SIMULATE && tempControl.ambientSensor->isConnected()
-		displayRoom = (((int(ticks.seconds())&0x04)==0)
-				 and tempControl.ambientSensor is not None)
-		if (displayRoom ^ ((flags & LCD_FLAG_DISPLAY_ROOM)!=0)):	# transition
-			flags = (flags | LCD_FLAG_DISPLAY_ROOM) if displayRoom else (flags & ~LCD_FLAG_DISPLAY_ROOM)
-			printStationaryText()
-	
-	printBeerTemp()
-	printBeerSet()
-	printFridgeTemp()
-	printFridgeSet()
+    # alternate between beer and room temp
+    if (flags & LCD_FLAG_ALTERNATE_ROOM):
+    #   bool displayRoom = ((ticks.seconds()&0x08)==0) && !BREWPI_SIMULATE && tempControl.ambientSensor->isConnected()
+        displayRoom = (((int(ticks.seconds())&0x04)==0)
+                and tempControl.ambientSensor is not None)
+        if (displayRoom ^ ((flags & LCD_FLAG_DISPLAY_ROOM)!=0)):    # transition
+            flags = (flags | LCD_FLAG_DISPLAY_ROOM) if displayRoom else (flags & ~LCD_FLAG_DISPLAY_ROOM)
+            printStationaryText()
+
+    printBeerTemp()
+    printBeerSet()
+    printFridgeTemp()
+    printFridgeSet()
 
 
 def printBeerTemp():
-	printTemperatureAt(6, 1, tempControl.temp_convert_to_external(tempControl.getBeerTemp()))
+    printTemperatureAt(6, 1, tempControl.temp_convert_to_external(tempControl.getBeerTemp()))
 
 
 def printBeerSet():
-	printTemperatureAt(12, 1, tempControl.temp_convert_to_external(tempControl.getBeerSetting()))
+    printTemperatureAt(12, 1, tempControl.temp_convert_to_external(tempControl.getBeerSetting()))
 
 
 def printFridgeTemp():
-	printTemperatureAt(6,2, tempControl.temp_convert_to_external(tempControl.ambientSensor.temperature)
-					if (flags & LCD_FLAG_DISPLAY_ROOM)
-					else tempControl.temp_convert_to_external(tempControl.getFridgeTemp()))
+    printTemperatureAt(6,2, tempControl.temp_convert_to_external(tempControl.ambientSensor.temperature)
+                    if (flags & LCD_FLAG_DISPLAY_ROOM)
+                    else tempControl.temp_convert_to_external(tempControl.getFridgeTemp()))
 
 
 def printFridgeSet():
-	fridgeSet = tempControl.temp_convert_to_external(tempControl.getFridgeSetting())
-	if (flags & LCD_FLAG_DISPLAY_ROOM):	# beer setting is not active
-		fridgeSet = None
-	printTemperatureAt(12, 2, fridgeSet)
+    fridgeSet = tempControl.temp_convert_to_external(tempControl.getFridgeSetting())
+    if (flags & LCD_FLAG_DISPLAY_ROOM): # beer setting is not active
+        fridgeSet = None
+    printTemperatureAt(12, 2, fridgeSet)
 
 
 def printTemperatureAt(x, y, temp):
-	LCD.cursor(x,y)
-	printTemperature(temp)
+    LCD.cursor(x,y)
+    printTemperature(temp)
 
 
 def printAt(x, y, s):
-	LCD.printat(x,y,s)
+    LCD.printat(x,y,s)
 
 
 def printTemperature(temp):
-	if (temp is None) or math.isnan(temp): #(isDisabledOrInvalid(temp)):
-		LCD.print(" --.-")
-		return
+    if (temp is None) or math.isnan(temp): #(isDisabledOrInvalid(temp)):
+        LCD.print(" --.-")
+        return
 
-	LCD.print("%5.1f"%temp)
+    LCD.print("%5.1f"%temp)
 
 
 def setDisplayFlags(newFlags):
-	
-	global flags
-	
-	flags = newFlags
-	printStationaryText()
-	printAllTemperatures()
+
+    global flags
+
+    flags = newFlags
+    printStationaryText()
+    printAllTemperatures()
 
 
 def getDisplayFlags():
-	return flags
+    return flags
 
 
-def printAll():	# FIXME: This doesn't seem to be used.
-	printStationaryText()
-	printState()
-	printAllTemperatures()
-	printMode()
+def printAll(): # FIXME: This doesn't seem to be used.
+    printStationaryText()
+    printState()
+    printAllTemperatures()
+    printMode()
 
 
 def update():
-	'''Call the update() function to copy the display contents to the hardware.'''
-	LCD.update()
+    '''Call the update() function to copy the display contents to the hardware.'''
+    LCD.update()
 
 
 #def resetBacklightTimer():
-#	lcd.resetBacklightTimer()
+#   lcd.resetBacklightTimer()
 
 
 #def updateBacklight():
-#	lcd.updateBacklight()
+#   lcd.updateBacklight()
